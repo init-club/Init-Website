f542d9b441f8c918eb0e15d9c9ca33921d8e7ea1
U-Naveen <kaveen0608@gmail.com>
2026-01-23 23:21:01 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import { GitGraph } from "../components/Graph/GitGraph";
+import { Footer } from "../components/Footer";
+import AboutWhatWeDo from "../components/AboutWhatWeDo";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+export function HomePage() {
+  return (
+    <>
+      <GitGraph />
+      <AboutWhatWeDo />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
